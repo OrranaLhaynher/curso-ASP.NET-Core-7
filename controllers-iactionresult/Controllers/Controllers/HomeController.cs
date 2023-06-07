815471204cb679257a458666b5b0baa7e3dc0d5e
Orrana Lhaynher <42912102+OrranaLhaynher@users.noreply.github.com>
2023-06-07 13:35:24 -0300

    Creating controllers

---
+++ controllers-iactionresult/Controllers/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Controllers.Controllers
+{
+    //Adicionar -Controller já transforma essa classe em um controller
+    public class HomeController
+    {
+        [Route("/")] //chamado de attribute routing, seta a rota do método abaixo
+        public string home()
+        {
+            return "Hello from method 1";
+        }
+
+        [Route("/login")]
+        public string login()
+        {
+            return "Página de login";
+        }
+
+        [Route("/blog")]
+        public string blog()
+        {
+            return "Página do blog";
+        }
+    }
+}
--- controllers-iactionresult/Controllers/Program.cs
+++ controllers-iactionresult/Controllers/Program.cs
@@ -1,6 +1,18 @@
+//using Controllers.Controllers;
+
 var builder = WebApplication.CreateBuilder(args);
+//builder.Services.AddTransient<HomeController>(); //forma de adicionar controllers manualmente um por um
+builder.Services.AddControllers(); //adiciona todos os controllers existentes no projeto automaticamente como serviços
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapControllers();
+//O app.UseRouting() e app.UseEndpoints() são opcionais, pois se vc usa o app.MapControllers() ele chama esses dois automaticamente
+//app.UseRouting();
+
+//app.UseEndpoints(endpoints =>
+//{
+//    endpoints.MapControllers();
+//});
 
 app.Run();

69b267c3e0b3bc65de4d2d7df0b0a79971b457e2
Orrana Lhaynher <42912102+OrranaLhaynher@users.noreply.github.com>
2023-06-07 14:28:00 -0300

    ContentResult

--- controllers-iactionresult/Controllers/Controllers/HomeController.cs
+++ controllers-iactionresult/Controllers/Controllers/HomeController.cs
@@ -1,21 +1,49 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Mime;
 
 namespace Controllers.Controllers
 {
     //Adicionar -Controller já transforma essa classe em um controller
-    public class HomeController
+    //Se não quiser adicionar o -Controller, pode colocar na linha acima da classe o atributo [Controller]
+    public class HomeController : Controller
     {
         [Route("/")] //chamado de attribute routing, seta a rota do método abaixo
         //[Route("/sayhello")] //pode adicionar várias rotas para o mesmo método
-        public string Index()
+        public ContentResult Index()
         {
-            return "Home page";
+            return new ContentResult()
+            {
+                Content = "<h1>Home</h1>",
+                ContentType = "text/html"
+            };
         }
 
         [Route("/about")] //chamado de attribute routing, seta a rota do método abaixo
-        public string About()
+        public ContentResult About()
         {
-            return "About page";
+            ContentResult result = new ContentResult()
+            {
+                Content = "<h1>About page<h1><h2>Informacoes sobre o produto aqui<h2>",
+                ContentType = "text/html"
+        };
+            return result;
+        }
+
+        [Route("/login")] //chamado de attribute routing, seta a rota do método abaixo
+        public ContentResult Login()
+        {
+            ContentResult result = new ContentResult();
+            result.Content = "<h1>Login page<h1><h2>Entre com suas informacoes de login aqui<h2>";
+            result.ContentType = "text/html";
+
+            return result;
+        }
+
+        [Route("/register")] //chamado de attribute routing, seta a rota do método abaixo
+        public ContentResult Register()
+        {
+
+            return Content("<h1>Pagina de cadastro</h1>", "text/html");
         }
 
         [Route("/contact/{mobile:regex(^\\d{{10}}$)}")] //chamado de attribute routing, seta a rota do método abaixo

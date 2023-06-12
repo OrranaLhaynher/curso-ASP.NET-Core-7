d97446a85402de4dafc2e2bbfcb1e79dd8bba9c1
Orrana Lhaynher <42912102+OrranaLhaynher@users.noreply.github.com>
2023-06-12 12:37:01 -0300

    Model state

--- model-binding-and-validation/ModelBindingAndValidation/Controllers/HomeController.cs
+++ model-binding-and-validation/ModelBindingAndValidation/Controllers/HomeController.cs
@@ -14,6 +14,26 @@ namespace Controllers.Controllers
         [Route("/register")]
         public IActionResult Register(Person person)
         {
+
+            if (!ModelState.IsValid)
+            {
+                //List<string> errorsList = new List<string>();
+
+                //foreach (var value in ModelState.Values)
+                //{
+                //    foreach (var error in value.Errors)
+                //    {
+                //        errorsList.Add(error.ErrorMessage);
+                //    }
+                //}
+
+                //string errors = string.Join("\n", errorsList);
+
+                //With LINQ
+                string errors = string.Join("\n",
+                    ModelState.Values.SelectMany(value => value.Errors).Select(error => error.ErrorMessage));
+                return BadRequest(errors);
+            }
             return Content($"<h1>{person}</h1>", "text/html");
         }
     }
--- model-binding-and-validation/ModelBindingAndValidation/Models/Person.cs
+++ model-binding-and-validation/ModelBindingAndValidation/Models/Person.cs
@@ -4,12 +4,18 @@ namespace ModelBindingAndValidation.Models
 {
     public class Person
     {
-        [Required] //signifa que a propriedade name é obrigatoria
+        //custom error message
+        [Required(ErrorMessage = "O nome é um campo obrigatório")] //signifa que a propriedade name é obrigatoria
         public string? Name { get; set; }
+        [Required(ErrorMessage = "O email é um campo obrigatório")]
         public string? Email { get; set; }
+        [Required(ErrorMessage = "O número de telefone é um campo obrigatório")]
         public string? Phone { get; set; }
+        [Required(ErrorMessage = "A senha é um campo obrigatório")]
         public string? Password { get; set; }
+        [Required(ErrorMessage = "A confirmação da senha é um campo obrigatório")]
         public string? ConfirmPassword { get; set; }
+        [Required(ErrorMessage = "O preço é um campo obrigatório")]
         public double? Price { get; set; }
 
         public override string ToString()
@@ -19,3 +25,8 @@ namespace ModelBindingAndValidation.Models
 
     }
 }
+
+//Model State
+//IsValid - specifies whether there is at least one validation error or not (true or false)
+//Values - contains each model property value with corresponding "Errors" property that contains list of validation errors of that model property
+//ErrorCount - returns number of errors

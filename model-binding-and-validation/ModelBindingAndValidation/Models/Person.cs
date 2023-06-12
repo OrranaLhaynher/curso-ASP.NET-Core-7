eec78d1fe4b75b0787d01d33b72cbd7298d81ddc
Orrana Lhaynher <42912102+OrranaLhaynher@users.noreply.github.com>
2023-06-12 13:11:34 -0300

    Model validations - part 2

--- model-binding-and-validation/ModelBindingAndValidation/Models/Person.cs
+++ model-binding-and-validation/ModelBindingAndValidation/Models/Person.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
 
 namespace ModelBindingAndValidation.Models
 {
@@ -8,18 +9,23 @@ namespace ModelBindingAndValidation.Models
         [Required(ErrorMessage = "O {0} é um campo obrigatório")] //{0} pega o nome da propriedade 
         [Display(Name = "Nome")] //funciona pra toda a propriedade, independente da ordem de adição
         [StringLength(45, MinimumLength = 3, ErrorMessage = "O campo {0} deve ter entre {2} e {1} caracteres")]
+        [RegularExpression("^[A-Za-z ]+$", ErrorMessage = "O campo {0} deve ter apenas caracteres alfabeticos e espaços")]
         public string? Name { get; set; }
 
-        [Required(ErrorMessage = "O email é um campo obrigatório")]
+        [Required(ErrorMessage = "O {0} é um campo obrigatório")]
+        [EmailAddress(ErrorMessage = "O campo {0} deve estar em um formato valido")]
         public string? Email { get; set; }
 
         [Required(ErrorMessage = "O número de telefone é um campo obrigatório")]
+        [Phone(ErrorMessage = "O campo {0} deve estar em um formato valido")]
+        [ValidateNever] //stop the validation in this property
         public string? Phone { get; set; }
 
         [Required(ErrorMessage = "A senha é um campo obrigatório")]
         public string? Password { get; set; }
 
         [Required(ErrorMessage = "A confirmação da senha é um campo obrigatório")]
+        [Compare("Password", ErrorMessage = "{0} deve ser igual ao campo {1}")]
         public string? ConfirmPassword { get; set; }
 
         [Required(ErrorMessage = "O preço é um campo obrigatório")]
@@ -28,7 +34,7 @@ namespace ModelBindingAndValidation.Models
 
         public override string ToString()
         {
-            return $"Person name - {Name}, Person email - {Email}, Person password - {Password}, Person confirm password - {ConfirmPassword}, Person price - {Price}";
+            return $"Person name - {Name}, Person email - {Email}, Person phone - {Phone}, Person password - {Password}, Person confirm password - {ConfirmPassword}, Person price - {Price}";
         }
 
     }
@@ -38,3 +44,5 @@ namespace ModelBindingAndValidation.Models
 //IsValid - specifies whether there is at least one validation error or not (true or false)
 //Values - contains each model property value with corresponding "Errors" property that contains list of validation errors of that model property
 //ErrorCount - returns number of errors
+
+//[Url(ErrorMessage = "Algo")]

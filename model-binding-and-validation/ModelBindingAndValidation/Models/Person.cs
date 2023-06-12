418a034d74ce2a2c6f3f0db15f6bfb9e32ee61d5
Orrana Lhaynher <42912102+OrranaLhaynher@users.noreply.github.com>
2023-06-12 12:51:00 -0300

    Model validations - part 1

--- model-binding-and-validation/ModelBindingAndValidation/Models/Person.cs
+++ model-binding-and-validation/ModelBindingAndValidation/Models/Person.cs
@@ -5,17 +5,25 @@ namespace ModelBindingAndValidation.Models
     public class Person
     {
         //custom error message
-        [Required(ErrorMessage = "O nome é um campo obrigatório")] //signifa que a propriedade name é obrigatoria
+        [Required(ErrorMessage = "O {0} é um campo obrigatório")] //{0} pega o nome da propriedade 
+        [Display(Name = "Nome")] //funciona pra toda a propriedade, independente da ordem de adição
+        [StringLength(45, MinimumLength = 3, ErrorMessage = "O campo {0} deve ter entre {2} e {1} caracteres")]
         public string? Name { get; set; }
+
         [Required(ErrorMessage = "O email é um campo obrigatório")]
         public string? Email { get; set; }
+
         [Required(ErrorMessage = "O número de telefone é um campo obrigatório")]
         public string? Phone { get; set; }
+
         [Required(ErrorMessage = "A senha é um campo obrigatório")]
         public string? Password { get; set; }
+
         [Required(ErrorMessage = "A confirmação da senha é um campo obrigatório")]
         public string? ConfirmPassword { get; set; }
+
         [Required(ErrorMessage = "O preço é um campo obrigatório")]
+        [Range(0, 1000.00, ErrorMessage = "O {0} deve ter valores entre R${1} e R${2}")]
         public double? Price { get; set; }
 
         public override string ToString()

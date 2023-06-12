e58d47de0b3b0ba9c87471ceff1a1e394aa71927
Orrana Lhaynher <42912102+OrranaLhaynher@users.noreply.github.com>
2023-06-12 11:25:06 -0300

    Model class

--- model-binding-and-validation/ModelBindingAndValidation/Models/Book.cs
+++ model-binding-and-validation/ModelBindingAndValidation/Models/Book.cs
@@ -1,9 +1,13 @@
-﻿namespace ModelBindingAndValidation.Models
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ModelBindingAndValidation.Models
 {
     public class Book
     {
+        // if we want to set one of the parameters to FromQuery/FromRoute add [FromRoute] before the parameter
         public int? BookId { get; set; }
 
+        [FromQuery]
         public string? Author { get; set; }
         //public bool? IsLoggedIn { get; set; }
 

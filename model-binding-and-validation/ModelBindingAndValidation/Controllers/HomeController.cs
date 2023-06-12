de162561c2a0c6e6603191c31d766282b88668a6
Orrana Lhaynher <42912102+OrranaLhaynher@users.noreply.github.com>
2023-06-12 12:11:17 -0300

    Introduction to model validations

--- model-binding-and-validation/ModelBindingAndValidation/Controllers/HomeController.cs
+++ model-binding-and-validation/ModelBindingAndValidation/Controllers/HomeController.cs
@@ -5,67 +5,16 @@ namespace Controllers.Controllers
 {
     public class HomeController : Controller
     {
-        [Route("/bookstore/{bookid?}/{isloggedin?}")] //model binding with route date
-        //We can set the Book book with FromQuery/FromRoute with => [FromQuery] Book book
-        //form-urlencoded - menos de 10 parametros no formulario (formularios simples)
-        //form-data - 10 a mais campos no formulario e anexos de arquivos (formularios mais complexos)
-        public IActionResult Book(int? bookid, bool? isloggedin, Book book) //model binding with query string parameters
+        [Route("/")]
+        public IActionResult Index()
         {
-            if (book.BookId == null)
-            {
-                return BadRequest("Book id can't be null or empty");
-            }
-
-            if (book.BookId <= 0)
-            {
-                return BadRequest("Book id is not valid! It cannot be less than or equal to 0.");
-            }
-
-            if (book.BookId > 1000)
-            {
-                return NotFound("Book id is not valid! It cannot be greater than 1000."); 
-            }
-
-            if (book.IsLoggedIn == null)
-            {
-                return StatusCode(401, "The confirmation of logged in can't be null or empty");
-            }
-
-            if (book.IsLoggedIn == false)
-            {
-                return Unauthorized("User must be logged in");
-            }
-
-            return Content($"<h1>{book.ToString()}</h1>", "text/html");
-        }
-
-        [Route("/about")]
-        public IActionResult About()
-        {
-            ContentResult result = new ContentResult()
-            {
-                Content = "<h1>About page<h1><h2>Informacoes sobre o produto aqui<h2>",
-                ContentType = "text/html"
-            };
-            return result;
-        }
-
-        [Route("/login")]
-        public IActionResult Login()
-        {
-            ContentResult result = new ContentResult();
-            result.Content = "<h1>Login page<h1><h2>Entre com suas informacoes de login aqui<h2>";
-            result.ContentType = "text/html";
-
-            return result;
+            return Content("<h1>Pagina Home</h1>", "text/html");
         }
 
         [Route("/register")]
-        public IActionResult Register()
+        public IActionResult Register(Person person)
         {
-
-            return Content("<h1>Pagina de cadastro</h1>", "text/html");
+            return Content($"<h1>{person}</h1>", "text/html");
         }
-
     }
 }
---
+++ model-binding-and-validation/ModelBindingAndValidation/Models/Person.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModelBindingAndValidation.Models
+{
+    public class Person
+    {
+        [Required] //signifa que a propriedade name é obrigatoria
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public string? Password { get; set; }
+        public string? ConfirmPassword { get; set; }
+        public double? Price { get; set; }
+
+        public override string ToString()
+        {
+            return $"Person name - {Name}, Person email - {Email}, Person password - {Password}, Person confirm password - {ConfirmPassword}, Person price - {Price}";
+        }
+
+    }
+}

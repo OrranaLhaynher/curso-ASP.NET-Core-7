443a958e6940cf79cee8f0c174b29b8a2494b4c3
Orrana Lhaynher <42912102+OrranaLhaynher@users.noreply.github.com>
2023-06-14 11:25:27 -0300

    Input formatters - XML

--- model-binding-and-validation/ModelBindingAndValidation/Program.cs
+++ model-binding-and-validation/ModelBindingAndValidation/Program.cs
@@ -1,5 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
+builder.Services.AddControllers().AddXmlSerializerFormatters(); // to read request body in xml format
 var app = builder.Build();
 
 app.UseStaticFiles();

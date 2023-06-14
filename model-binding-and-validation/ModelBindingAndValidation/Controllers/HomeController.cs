2cebbf1ccceb3f77ac38f44165fc4a5b36e17adf
Orrana Lhaynher <42912102+OrranaLhaynher@users.noreply.github.com>
2023-06-14 12:40:50 -0300

    FromHeader

--- model-binding-and-validation/ModelBindingAndValidation/Controllers/HomeController.cs
+++ model-binding-and-validation/ModelBindingAndValidation/Controllers/HomeController.cs
@@ -18,29 +18,20 @@ namespace Controllers.Controllers
         //[Bind(nameof(Person.Name), nameof(Person.Email), nameof(Person.Password), nameof(Person.ConfirmPassword), nameof(Person.DateOfBirth))]
         //[Bind] é para setar quais atributos serão utilizados. Todos os atributos como padrão usam bind, mas se a lista de atributos é grande e para certa rota, não são necessários todos, é aí que entra o [Bind]
         //[ModelBinder(BinderType = typeof(PersonModelBinder))]
-        public IActionResult Register(Person person)
+        public IActionResult Register(Person person, [FromHeader(Name = "User-Agent")] string UserAgent)
         {
 
             if (!ModelState.IsValid)
             {
-                //List<string> errorsList = new List<string>();
-
-                //foreach (var value in ModelState.Values)
-                //{
-                //    foreach (var error in value.Errors)
-                //    {
-                //        errorsList.Add(error.ErrorMessage);
-                //    }
-                //}
-
-                //string errors = string.Join("\n", errorsList);
-
                 //With LINQ
                 string errors = string.Join("\n",
                     ModelState.Values.SelectMany(value => value.Errors).Select(error => error.ErrorMessage));
                 return BadRequest(errors);
             }
-            return Content($"{person}", "text/plain");
+
+            //ControllerContext.HttpContext.Request.Headers["key"]; //forma tradicional
+
+            return Content($"{person}, {UserAgent}", "text/plain");
         }
     }
 }

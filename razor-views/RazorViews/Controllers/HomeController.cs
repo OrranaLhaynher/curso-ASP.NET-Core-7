7aeb475d9a8e8edd414efbc00a1cf84834acab48
Orrana Lhaynher <42912102+OrranaLhaynher@users.noreply.github.com>
2023-06-15 12:15:04 -0300

    ViewData - parte 1

--- razor-views/RazorViews/Controllers/HomeController.cs
+++ razor-views/RazorViews/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RazorViews.Models;
+using static RazorViews.Models.Person;
 
 namespace RazorViews.Controllers
 {
@@ -7,6 +9,19 @@ namespace RazorViews.Controllers
         [Route("/")]
         public IActionResult Index()
         {
+            ViewData["appTitle"] = "ASP.NET Core Demo APP - Usando ViewData";
+
+            List<Person> people = new List<Person>()
+            {
+                new Person() { Name = "John", DateOfBirth = DateTime.Parse("1988-07-09"), PersonGender = Gender.Male },
+                new Person() { Name = "Samantha", PersonGender = Gender.Male },
+                new Person() { Name = "Chris", DateOfBirth = DateTime.Parse("2001-04-02"), PersonGender = Gender.Other },
+                new Person() { Name = "Michelle", DateOfBirth = DateTime.Parse("1970-01-14"), PersonGender = Gender.Female },
+                new Person() { Name = "Mickayla", DateOfBirth = DateTime.Parse("2004-12-05"), PersonGender = Gender.Other }
+            };
+
+            ViewData["people"] = people;
+
             return View();
         }
     }

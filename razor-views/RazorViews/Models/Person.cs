23125b538579d2eef61f54b6beda45c12cc70604
Orrana Lhaynher <42912102+OrranaLhaynher@users.noreply.github.com>
2023-06-14 21:37:11 -0300

    Switch case in .cshtml

--- razor-views/RazorViews/Models/Person.cs
+++ razor-views/RazorViews/Models/Person.cs
@@ -4,6 +4,12 @@
     {
         public string? Name { get; set; }
         public DateTime? DateOfBirth { get; set; }
+        public Gender PersonGender { get; set; }
+
+        public enum Gender
+        {
+            Male, Female, Other
+        }
 
     }
 }

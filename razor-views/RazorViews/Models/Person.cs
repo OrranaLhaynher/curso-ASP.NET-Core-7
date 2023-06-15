655ef3b77503a5a36fcdc15d4c77a1ca1869cd3e
Orrana Lhaynher <42912102+OrranaLhaynher@users.noreply.github.com>
2023-06-14 21:25:10 -0300

    If-else in .cshtml

--- razor-views/RazorViews/Models/Person.cs
+++ razor-views/RazorViews/Models/Person.cs
@@ -3,7 +3,7 @@
     public class Person
     {
         public string? Name { get; set; }
-        public DateTime DateOfBirth { get; set; }
+        public DateTime? DateOfBirth { get; set; }
 
     }
 }

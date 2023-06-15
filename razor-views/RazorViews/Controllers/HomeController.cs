8f9ce62efc719131ec72befd864af227f77de1da
Orrana Lhaynher <42912102+OrranaLhaynher@users.noreply.github.com>
2023-06-15 14:19:52 -0300

    Strong typed views - part 2

--- razor-views/RazorViews/Controllers/HomeController.cs
+++ razor-views/RazorViews/Controllers/HomeController.cs
@@ -9,7 +9,7 @@ namespace RazorViews.Controllers
         [Route("/")]
         public IActionResult Index()
         {
-            ViewData["appTitle"] = "ASP.NET Core Demo APP - Usando ViewData";
+            //ViewData["appTitle"] = "ASP.NET Core Demo APP - Usando ViewData";
 
             List<Person> people = new List<Person>()
             {
@@ -23,6 +23,30 @@ namespace RazorViews.Controllers
             //ViewData["people"] = people;
 
             return View("Index", people);
+        }
+
+        [Route("/details/{name?}")]
+        public IActionResult Details(string? name)
+        {
+            if (name == null)
+            {
+                return Content("Person name can't be null");
+            }
+            else
+            {
+                List<Person> people = new List<Person>()
+                {
+                    new Person() { Name = "John", DateOfBirth = DateTime.Parse("1988-07-09"), PersonGender = Gender.Male },
+                    new Person() { Name = "Samantha", PersonGender = Gender.Male },
+                    new Person() { Name = "Chris", DateOfBirth = DateTime.Parse("2001-04-02"), PersonGender = Gender.Other },
+                    new Person() { Name = "Michelle", DateOfBirth = DateTime.Parse("1970-01-14"), PersonGender = Gender.Female },
+                    new Person() { Name = "Mickayla", DateOfBirth = DateTime.Parse("2004-12-05"), PersonGender = Gender.Other }
+                };
+
+                Person? matchingPerson = people.Where(temp => temp.Name?.ToLower() == name.ToLower()).FirstOrDefault();
+
+                return View("../Details/Index", matchingPerson);
+            }
         }
     }
 }
---
+++ razor-views/RazorViews/Views/Details/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorViews.Views.Details
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

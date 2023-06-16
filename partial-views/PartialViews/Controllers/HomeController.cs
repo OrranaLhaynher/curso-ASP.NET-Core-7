e57e47c4c06e6ef003ca20cad0712dd4c5b0051a
Orrana Lhaynher <42912102+OrranaLhaynher@users.noreply.github.com>
2023-06-16 16:35:35 -0300

    Creating partial views

---
+++ partial-views/PartialViews/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PartialViews.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            ViewData["title"] = "Home";
+            return View();
+        }
+
+        [Route("/about")]
+        public IActionResult About()
+        {
+            ViewData["title"] = "About";
+            return View();
+        }
+    }
+}

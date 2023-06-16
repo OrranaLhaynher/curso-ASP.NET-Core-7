dbe421faea8dc830704f2c6d843dacab1c1579cd
Orrana Lhaynher <42912102+OrranaLhaynher@users.noreply.github.com>
2023-06-16 19:10:09 -0300

    Strongly typed partial views

---
+++ partial-views/PartialViews/Models/ListModel.cs
@@ -0,0 +1,8 @@
+﻿namespace PartialViews.Models
+{
+    public class ListModel
+    {
+        public string ListTitle { get; set; } = "";
+        public List<string> ListItems { get; set;} = new List<string>();
+    }
+}

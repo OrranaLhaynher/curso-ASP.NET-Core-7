5d52a9c7ca0e742ff91887bb4a2a624d568f8f2f
Orrana Lhaynher <42912102+OrranaLhaynher@users.noreply.github.com>
2023-06-16 18:04:46 -0300

    Partial views with ViewData

--- partial-views/PartialViews/Controllers/HomeController.cs
+++ partial-views/PartialViews/Controllers/HomeController.cs
@@ -8,6 +8,16 @@ namespace PartialViews.Controllers
         public IActionResult Index()
         {
             ViewData["title"] = "Home";
+            ViewData["listTitle"] = "Cities";
+            ViewData["listItems"] = new List<string>()
+            {
+                "Paris",
+                "New York",
+                "Buenos Aires",
+                "Teresina",
+                "Tokio",
+                "Seul"
+            };
             return View();
         }
 

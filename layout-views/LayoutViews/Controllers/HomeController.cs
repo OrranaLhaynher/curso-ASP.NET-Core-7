3ddb880e84e73de72ae5e0b714d45485abec0432
Orrana Lhaynher <42912102+OrranaLhaynher@users.noreply.github.com>
2023-06-16 09:26:11 -0300

    Creating layout views - part 1

---
+++ layout-views/LayoutViews/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LayoutViews.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("/about")]
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        [Route("/contact")]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
---
+++ layout-views/LayoutViews/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LayoutViews.Controllers
+{
+    public class ProductController : Controller
+    {
+        [Route("/products")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("/products/search")]
+        public IActionResult Search()
+        {
+            return View();
+        }
+
+        [Route("/products/order")]
+        public IActionResult Order()
+        {
+            return View();
+        }
+    }
+}

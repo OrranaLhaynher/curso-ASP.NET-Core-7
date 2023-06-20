f2c8bb74f6a613ff3196a9d165d8fff3fc06e063
Orrana Lhaynher <42912102+OrranaLhaynher@users.noreply.github.com>
2023-06-20 10:43:34 -0300

    Parte 1 e 2 - creating view components

---
+++ view-components/ViewComponents/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ViewComponents.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Home";
+            return View();
+        }
+
+        [Route("/about")]
+        public IActionResult About()
+        {
+            ViewData["Title"] = "About";
+            return View();
+        }
+    }
+}
---
+++ view-components/ViewComponents/ViewComponents/GridViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ViewComponents.ViewComponents
+{
+    //[ViewComponent]
+    public class GridViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View(); //invoke a partial view -> ~/Views/Shared/Components/Grid/Default.cshtml
+        }
+    }
+}

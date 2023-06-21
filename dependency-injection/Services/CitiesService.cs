e232ec308d1fb40a116523aed1c6f5921dcdb02a
Orrana Lhaynher <42912102+OrranaLhaynher@users.noreply.github.com>
2023-06-21 12:59:32 -0300

    Services - part 1

---
+++ dependency-injection/Services/CitiesService.cs
@@ -0,0 +1,24 @@
+﻿namespace Services
+{
+    public class CitiesService
+    {
+        private List<string> _cities; //é recomendando adicionar o _ antes do nome de propriedades privadas
+
+        public CitiesService() 
+        { 
+            _cities = new List<string>()
+            {
+                "London",
+                "Paris",
+                "New York",
+                "Tokyo",
+                "Rome"
+            };
+        }
+
+        public List<string> GetCities()
+        {
+            return _cities;
+        }
+    }
+}

3e52fae32d26c5256221b840cfdd8eddae6251d6
Orrana Lhaynher <42912102+OrranaLhaynher@users.noreply.github.com>
2023-06-21 12:12:39 -0300

    Weather App with View Components

---
+++ weather-app/WeatherApp/ViewComponents/WeatherViewComponent.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WeatherApp.Models;
+
+namespace WeatherApp.ViewComponents
+{
+    public class WeatherViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(CityWeather city)
+        {
+            ViewData["TemperatureColor"] = ChooseColor(city.TemperatureFahrenheit);
+            return View(city);
+        }
+
+        public string ChooseColor(int temperature)
+        {
+            if (temperature < 44)
+            {
+                return "#AFD5F0";
+            }
+            else if (temperature >= 44 && temperature <= 74)
+            {
+                return "#77dd77";
+            }
+            else
+            {
+                return "#FAC898";
+            }
+        }
+    }
+}

0bcad4433952d63bd203c62e13cb4dda7bffec64
Orrana Lhaynher <42912102+OrranaLhaynher@users.noreply.github.com>
2023-06-21 11:21:41 -0300

    View components with parameters

--- view-components/ViewComponents/ViewComponents/GridViewComponent.cs
+++ view-components/ViewComponents/ViewComponents/GridViewComponent.cs
@@ -6,20 +6,20 @@ namespace ViewComponents.ViewComponents
     //[ViewComponent]
     public class GridViewComponent : ViewComponent
     {
-        public async Task<IViewComponentResult> InvokeAsync()
+        public async Task<IViewComponentResult> InvokeAsync(PersonGridModel grid)
         {
-            PersonGridModel grid = new PersonGridModel()
-            {
-                GridTitle = "Persons List",
-                Persons = new List<Person>()
-                {
-                    new Person() {Name="Orrana Lhaynher", JobTitle="Software Developer", JobDescription="Develops software with differents languages"},
-                    new Person() {Name="Lee Taemin", JobTitle="Kpop Idol", JobDescription="Kpop singer that has multiple talents"},
-                    new Person() {Name="Joaquim da Vaquejada", JobTitle="Vaqueiro", JobDescription="Já fiquei com preguiça"},
-                    new Person() {Name="Orraniely Veloso", JobTitle="Students", JobDescription="Studes to be someone"},
-                    new Person() {Name="Lula da Silva", JobTitle="Brazil's president", JobDescription="Governs Brazil"},
-                }
-            };
+            //PersonGridModel grid = new PersonGridModel()
+            //{
+            //    GridTitle = "Persons List",
+            //    Persons = new List<Person>()
+            //    {
+            //        new Person() {Name="Orrana Lhaynher", JobTitle="Software Developer", JobDescription="Develops software with differents languages"},
+            //        new Person() {Name="Lee Taemin", JobTitle="Kpop Idol", JobDescription="Kpop singer that has multiple talents"},
+            //        new Person() {Name="Joaquim da Vaquejada", JobTitle="Vaqueiro", JobDescription="Já fiquei com preguiça"},
+            //        new Person() {Name="Orraniely Veloso", JobTitle="Students", JobDescription="Studes to be someone"},
+            //        new Person() {Name="Lula da Silva", JobTitle="Brazil's president", JobDescription="Governs Brazil"},
+            //    }
+            //};
 
             //ViewData["personList"] = grid;
 

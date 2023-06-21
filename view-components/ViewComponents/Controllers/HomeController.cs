8cf6692396d1f0496e1c20f9afefbc8a4f1a3494
Orrana Lhaynher <42912102+OrranaLhaynher@users.noreply.github.com>
2023-06-21 11:41:09 -0300

    ViewComponentResult

--- view-components/ViewComponents/Controllers/HomeController.cs
+++ view-components/ViewComponents/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ViewComponents.Models;
 
 namespace ViewComponents.Controllers
 {
@@ -16,6 +17,24 @@ namespace ViewComponents.Controllers
         {
             ViewData["Title"] = "About";
             return View();
+        }
+
+        [Route("/load-persons-list")]
+        public IActionResult LoadPersonsList()
+        {
+            PersonGridModel personModel = new PersonGridModel()
+            {
+                GridTitle = "Persons",
+                Persons = new List<Person>()
+                {
+                    new Person() { Name="Maria", JobTitle="Teacher", JobDescription="She teaches to small kids" },
+                    new Person() { Name="Osvaldo", JobTitle="Freelancer", JobDescription="He does what he wants" },
+                    new Person() { Name="Mario", JobTitle="Manager", JobDescription="He manage things" },
+
+                }
+            };
+
+            return ViewComponent("Grid", personModel);
         }
     }
 }
---
+++ view-components/ViewComponents/wwwroot/persons.js
@@ -0,0 +1,6 @@
+﻿document.querySelector("#load-persons-list").addEventListener("click",
+    async function () {
+        var response = await fetch("load-persons-list", { method: "GET" });
+        var responseBody = await response.text();
+        document.querySelector("#list").innerHTML = responseBody;
+});

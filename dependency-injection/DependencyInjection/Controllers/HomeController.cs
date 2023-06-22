373493742e93811ed93ea74ac1687171c293c5bb
Orrana Lhaynher <42912102+OrranaLhaynher@users.noreply.github.com>
2023-06-22 10:34:52 -0300

    Method injection - from service

--- dependency-injection/DependencyInjection/Controllers/HomeController.cs
+++ dependency-injection/DependencyInjection/Controllers/HomeController.cs
@@ -5,16 +5,17 @@ namespace DependencyInjection.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ICitiesService _citiesService;
+        //private readonly ICitiesService _citiesService;
 
-        public HomeController(ICitiesService citiesService)
-        {
-            //create object of CitiesService class
-            _citiesService = citiesService;
-        }
+        //mais comumente usando do que o method injection
+        //public HomeController(ICitiesService citiesService) //chamado de constructor injection
+        //{
+        //    //create object of CitiesService class
+        //    _citiesService = citiesService;
+        //}
 
         [Route("/")]
-        public IActionResult Index()
+        public IActionResult Index([FromServices] ICitiesService _citiesService) //chamado de method injection - quando não é necessário fazer a injection de todo o modelo para todos os métodos do controller e apenas a injection de um método único, como no Index aqui
         {
             List<string> cities = _citiesService.GetCities();
             return View(cities);

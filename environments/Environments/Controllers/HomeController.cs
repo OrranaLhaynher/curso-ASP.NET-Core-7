55a1db3646f14596ff4912c49e0a2f2fcb3cd687
Orrana Lhaynher <42912102+OrranaLhaynher@users.noreply.github.com>
2023-06-26 12:16:50 -0300

    Environment in launch settings

---
+++ environments/Environments/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Environments.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        [Route("/some-route")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("/some-route")]
+        public IActionResult SomeRoute()
+        {
+            return View();
+        }
+    }
+}

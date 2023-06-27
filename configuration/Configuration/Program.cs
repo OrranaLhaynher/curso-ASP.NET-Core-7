986e59b623336a0e3cc782e0888fdea10f5aec3c
Orrana Lhaynher <42912102+OrranaLhaynher@users.noreply.github.com>
2023-06-27 10:29:50 -0300

    Configuration basics

---
+++ configuration/Configuration/Program.cs
@@ -0,0 +1,20 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+var app = builder.Build();
+
+app.UseStaticFiles();
+app.UseRouting();
+app.UseEndpoints(endpoints => 
+{
+    endpoints.Map("/", async context =>
+    {
+        await context.Response.WriteAsync(app.Configuration["MyKey"] + "\n");
+
+        await context.Response.WriteAsync(app.Configuration.GetValue<string>("MyKey") + "\n");
+
+        await context.Response.WriteAsync(app.Configuration.GetValue<int>("x", 15) + "\n");
+    });
+});
+app.MapControllers();
+
+app.Run();

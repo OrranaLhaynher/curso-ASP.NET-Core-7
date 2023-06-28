adcd722af58a0769af05a5cc7ab1dd9d0e7405af
Orrana Lhaynher <42912102+OrranaLhaynher@users.noreply.github.com>
2023-06-28 08:51:35 -0300

    Configuration as service

--- configuration/Configuration/Controllers/HomeController.cs
+++ configuration/Configuration/Controllers/HomeController.cs
@@ -1,30 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 
 namespace Configuration.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly IConfiguration _configuration;
+        private readonly WeatherAPIOptions _options;
 
-        public HomeController(IConfiguration configuration)
+        public HomeController(IOptions<WeatherAPIOptions> configuration)
         {
-            _configuration = configuration;
+            _options = configuration.Value;
         }
         [Route("/")]
         public IActionResult Index()
         {
             ViewBag.Title = "Home";
-            //ViewBag.ClientID = _configuration["WeatherAPI:ClientID"];
-            //ViewBag.ClientSecretKey = _configuration.GetValue<string>("WeatherAPI:ClientSecretKey", "the default secret key");//_configuration.GetValue<string>("myName", "Orrana");
-
-            //Get
-            //WeatherAPIOptions option = _configuration.GetSection("WeatherAPI").Get<WeatherAPIOptions>();
-
-            //Bind
-            WeatherAPIOptions option = new WeatherAPIOptions();
-            _configuration.GetSection("WeatherAPI").Bind(option);
-            ViewBag.ClientID = option.ClientId;
-            ViewBag.ClientSecretKey = option.ClientSecretKey;
+            ViewBag.ClientID = _options.ClientId;
+            ViewBag.ClientSecretKey = _options.ClientSecretKey;
 
 
             return View();
--- configuration/Configuration/Program.cs
+++ configuration/Configuration/Program.cs
@@ -1,5 +1,10 @@
+using Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
+builder.Services.Configure<WeatherAPIOptions>(
+    builder.Configuration.GetSection("WeatherAPI")
+);
 var app = builder.Build();
 
 app.UseStaticFiles();

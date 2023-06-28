56817b7f4e64215dee449fb6264e1b3a577b345d
Orrana Lhaynher <42912102+OrranaLhaynher@users.noreply.github.com>
2023-06-28 16:44:17 -0300

    Http client - part 1

---
+++ stocks-app/StocksApp/Services/MyService.cs
@@ -0,0 +1,26 @@
+﻿namespace StocksApp.Services
+{
+    public class MyService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public MyService(IHttpClientFactory httpClientFactory) 
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task Request()
+        {
+            using(HttpClient httpClient = _httpClientFactory.CreateClient())
+            {
+                HttpRequestMessage httpRequestMessage = new HttpRequestMessage()
+                {
+                    Method = HttpMethod.Get,
+                    RequestUri = new Uri("")
+                };
+
+                HttpResponseMessage httpResponseMessage = await httpClient.SendAsync(httpRequestMessage);
+            }
+        }
+    }
+}

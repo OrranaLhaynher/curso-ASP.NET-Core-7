a6823453128a9ab08d64bb328bae3f6dbcc62f8f
Orrana Lhaynher <42912102+OrranaLhaynher@users.noreply.github.com>
2023-07-08 19:42:05 -0300

    Add person - implementation

--- crud-example/Services/PersonsService.cs
+++ crud-example/Services/PersonsService.cs
@@ -1,13 +1,47 @@
-﻿using ServiceContracts;
+﻿using Entities;
+using ServiceContracts;
 using ServiceContracts.DTO;
+using System.Diagnostics.Metrics;
 
 namespace Services
 {
     public class PersonsService : IPersonsService
     {
+        private readonly List<Person> _persons;
+        private readonly ICountriesService _countriesService;
+
+        public PersonsService()
+        {
+            _persons = new List<Person>();
+            _countriesService = new CountriesService();
+        }
+
+        private PersonResponse ConvertPersonToPersonResponse(Person person)
+        {
+            PersonResponse response = person.ToPersonResponse();
+            response.Country = _countriesService.GetCountry(person.CountryId)?.CountryName;
+
+            return response;
+        }
+
         public PersonResponse AddPerson(PersonAddRequest? personAddRequest)
         {
-            throw new NotImplementedException();
+            if (personAddRequest == null)
+            {
+                throw new ArgumentNullException(nameof(personAddRequest));
+            }
+
+            if (string.IsNullOrEmpty(personAddRequest.PersonName))
+            {
+                throw new ArgumentException("Person name cannot be blank");
+            }
+
+            Person person = personAddRequest!.ToPerson();
+
+            person.PersonId = Guid.NewGuid();
+            _persons.Add(person);
+
+            return ConvertPersonToPersonResponse(person);
         }
 
         public List<PersonResponse> GetAllPersons()

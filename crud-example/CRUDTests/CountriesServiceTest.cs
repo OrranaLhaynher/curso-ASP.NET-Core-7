c4837f8f622dafeb35efc00a6d017d7de27cc227
Orrana Lhaynher <42912102+OrranaLhaynher@users.noreply.github.com>
2023-07-08 13:09:44 -0300

    Add country - implementation

--- crud-example/CRUDTests/CountriesServiceTest.cs
+++ crud-example/CRUDTests/CountriesServiceTest.cs
@@ -27,5 +27,60 @@ namespace CRUDTests
                 _countriesService.AddCountry(countryAddRequest);
             });
         }
+
+        [Fact]
+        public void AddCountry_CountryNameIsNull()
+        {
+            //Arrange
+            CountryAddRequest? countryAddRequest = new CountryAddRequest()
+            {
+                CountryName = null
+            };
+
+            //Assert
+            Assert.Throws<ArgumentException>(() =>
+            {
+                //Act
+                _countriesService.AddCountry(countryAddRequest);
+            });
+        }
+
+        [Fact]
+        public void AddCountry_DuplicateCountryName()
+        {
+            //Arrange
+            CountryAddRequest? countryAddRequest = new CountryAddRequest()
+            {
+                CountryName = "USA"
+            };
+            CountryAddRequest? countryAddRequest2 = new CountryAddRequest()
+            {
+                CountryName = "USA"
+            };
+
+            //Assert
+            Assert.Throws<ArgumentException>(() =>
+            {
+                //Act
+                _countriesService.AddCountry(countryAddRequest);
+                _countriesService.AddCountry(countryAddRequest2);
+            });
+        }
+
+        [Fact]
+        public void AddCountry_ProperCountryDetails()
+        {
+            //Arrange
+            CountryAddRequest? countryAddRequest = new CountryAddRequest()
+            {
+                CountryName = "Japan"
+            };
+
+            //Act
+            CountryResponse response = _countriesService.AddCountry(countryAddRequest);
+
+            //Assert
+            Assert.True(response.CountryId != Guid.Empty);
+        }
     }
 }
--- crud-example/Services/CountriesService.cs
+++ crud-example/Services/CountriesService.cs
@@ -1,13 +1,42 @@
-﻿using ServiceContracts;
+﻿using Entities;
+using ServiceContracts;
 using ServiceContracts.DTO;
+using System.Linq;
 
 namespace Services
 {
     public class CountriesService : ICountriesService
     {
+        private readonly List<Country> _countries;
+
+        public CountriesService() 
+        { 
+            _countries = new List<Country>();
+        }
+
         public CountryResponse AddCountry(CountryAddRequest? countryAddRequest)
         {
-            throw new NotImplementedException();
+            if (countryAddRequest == null)
+            {
+                throw new ArgumentNullException(nameof(countryAddRequest));
+            }
+
+            if (countryAddRequest.CountryName == null) 
+            { 
+                throw new ArgumentException(nameof(countryAddRequest.CountryName));
+            }
+
+            if (_countries.Where(temp => temp.CountryName == countryAddRequest.CountryName).Count() > 0)
+            {
+                throw new ArgumentException("Given country name already exists");
+            }
+
+            Country country = countryAddRequest!.ToCountry();
+
+            country.CountryId = Guid.NewGuid();
+            _countries.Add(country);
+
+            return country.ToCountryResponse();
         }
     }
 }

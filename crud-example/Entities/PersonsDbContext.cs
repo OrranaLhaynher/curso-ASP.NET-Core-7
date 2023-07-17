5f8350d7bd892f0ea0c5c035105bc18fb8ed2f47
Orrana Lhaynher <42912102+OrranaLhaynher@users.noreply.github.com>
2023-07-17 17:14:54 -0300

    Migrations

--- crud-example/Entities/PersonsDbContext.cs
+++ crud-example/Entities/PersonsDbContext.cs
@@ -5,6 +5,8 @@ namespace Entities
 {
     public class PersonsDbContext : DbContext //cada um desses DbContext é um banco
     {
+        public PersonsDbContext(DbContextOptions options) : base(options) {  }
+
         public DbSet<Country> Countries { get; set; } //cada um desses é uma tabela
         public DbSet<Person> Persons { get; set; }
 
@@ -15,23 +17,21 @@ namespace Entities
             modelBuilder.Entity<Country>().ToTable("Countries");
             modelBuilder.Entity<Person>().ToTable("Persons");
 
-            //Seed data to Countries
+            //Seed to Countries
             string countriesJson = File.ReadAllText("countries.json");
-            List<Country> countries = JsonSerializer.Deserialize<List<Country>>(countriesJson)!;
+            List<Country> countries = JsonSerializer.Deserialize<List<Country>>(countriesJson);
 
             foreach (Country country in countries)
-            {
                 modelBuilder.Entity<Country>().HasData(country);
-            }
 
-            //Seed data to Persons
+
+            //Seed to Persons
             string personsJson = File.ReadAllText("persons.json");
-            List<Person> persons = JsonSerializer.Deserialize<List<Person>>(personsJson)!;
+            List<Person> persons = JsonSerializer.Deserialize<List<Person>>(personsJson);
 
             foreach (Person person in persons)
-            {
                 modelBuilder.Entity<Person>().HasData(person);
-            }
+
             //    modelBuilder.Entity<Country>().HasData(new Country()
             //    {
             //        CountryId = Guid.NewGuid(),
